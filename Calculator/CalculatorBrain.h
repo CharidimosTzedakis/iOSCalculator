8e9630ac938470d386e779915926d80f0ecfe6d5
Harris Tzedakis <harris.jedakis@gmail.com>
2012-01-24 10:35:05 +0200

    Initialization

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Harris Jedakis on 12/29/11.
+//  Copyright (c) 2011 -. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Harris Jedakis on 12/29/11.
+//  Copyright (c) 2011 -. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end
